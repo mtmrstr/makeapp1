16fdf7aceda0313d7cdaddc48de27772f9bf34b1
mtmrstr <106470191+mtmrstr@users.noreply.github.com>
2022-06-10 19:52:46 +0000

    {finish}4-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,4 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-
-Rails.application.routes.draw do
-  get 'lists/new'
-  get 'top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-
-end
-  Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists/edit'
-  get 'top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list'
-end
-  get 'lists/edit'
-  get 'homes/top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+ get "top" => 'home#top'
+ resources:lists
+  end
